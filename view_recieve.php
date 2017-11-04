32f9e9426ff4cdc09b3a31d05ce28f3b265c8cdf
Raghuvinayak Rao Medisetti <raghuvinayak99@gmail.com>
2017-11-04 14:09:03 +0100

    Create view_recieve.php

---
+++ view_recieve.php
@@ -0,0 +1,37 @@
+<?php include_once('include/header.php');?>
+</div>
+
+<br>
+<br>
+<div class="table-responsive">
+	<table class="table table-striped">
+   
+    <tr>
+     <td>File Name</td>
+	 <td>sender</td>
+     <td>View</td>
+    </tr>
+    <?php
+	$userId=$_SESSION['user'];
+	$sql="SELECT * FROM can_view where reciver=$userId";
+	$result_set=mysql_query($sql);
+	while($row=mysql_fetch_array($result_set))
+	{
+		?>
+			
+        <tr>
+        <td><?php echo $row['file_id'] ?></td>
+        <td><?php echo $row['sender_name'] ?></td>
+        <td><a href="uploads/<?php echo $row['file_id']?>" target="_blank">view file</a></td>
+        </tr>
+        <?php
+	}
+	?>
+    </table>
+    
+</div>
+ 
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
